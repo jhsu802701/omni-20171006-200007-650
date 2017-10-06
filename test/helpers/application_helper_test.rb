574d5f3b2b402c27b5bd72593e9507a8c4e3b106
Jason Hsu <jhsu802701@jasonhsu.com>
2017-10-06 20:10:11 +0000

    Initial commit

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'full title helper' do
+    assert_equal full_title, 'Temporary OmniAuth App'
+    assert_equal full_title('About'), 'About | Temporary OmniAuth App'
+    assert_equal full_title('Contact'), 'Contact | Temporary OmniAuth App'
+    assert_equal full_title('Michael Hartl'), 'Michael Hartl | Temporary OmniAuth App'
+  end
+end
